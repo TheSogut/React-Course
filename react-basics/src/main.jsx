95c7a38da083cb71c53dc53e009f97fbac8a3c14
Enes Söğüt <enessogut@outlook.com>
2025-12-11 13:18:17 +0300

    JSX Conditionals States

--- react-basics/src/main.jsx
+++ react-basics/src/main.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <Header />
       <ProductList />
+      <Footer />
     </>
   );
 }
@@ -21,41 +22,51 @@ function ProductList() {
       title: "IPhone 16",
       description: "Lorem ipsum dolor sit amet consectetur.",
       price: 80000,
+      is_active: false,
     },
     {
       image: "2.jpg",
       title: "IPhone 17",
       description: "Lorem ipsum dolor sit amet consectetur.",
       price: 90000,
+      is_active: true,
     },
     {
       image: "3.jpg",
       title: "IPhone 18",
       description: "Lorem ipsum dolor sit amet consectetur.",
       price: 100000,
+      is_active: true,
     },
     {
       image: "4.jpg",
       title: "IPhone 19",
       description: "Lorem ipsum dolor sit amet consectetur.",
       price: 110000,
+      is_active: true,
     },
   ];
+
+  // const items = [];
 
   return (
     <>
       <h2>Product List</h2>
-
-      {items.map((item, index) => (
-        <Product key={index} productObj={item} />
-      ))}
+      {items.length > 0 ? (
+        <div id="product-list">
+          {items.map((item, index) => (
+            <Product key={index} productObj={item} />
+          ))}
+        </div>
+      ) : (
+        <p>Şu anda satışta olan ürünümüz yok</p>
+      )}
     </>
   );
 }
 
 function Product({ productObj }) {
-  console.log(productObj);
-
+  if (!productObj.is_active) return null;
   return (
     <div>
       <img src={"/img/" + productObj.image} alt="" />
@@ -63,6 +74,23 @@ function Product({ productObj }) {
       <p>{productObj.description}</p>
       <span>{productObj.price}</span>
     </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 20;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer>
+      {isOpen ? (
+        <p>Akşam {closeHour}' e kadar sipariş verebilirsiniz.</p>
+      ) : (
+        <p>Şuan kapalıyız. Açılış saatimiz : {openHour}</p>
+      )}
+    </footer>
   );
 }
 
