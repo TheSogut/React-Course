4934233ae44fa1bb19a1b02b9fa4bb4875adaf54
Enes Söğüt <enessogut@outlook.com>
2025-12-12 12:28:34 +0300

    useState Function

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -1,29 +1,51 @@
+import { useState } from "react";
 import { sculptureList } from "./data.js";
 
 function App() {
-  let index = 5;
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
   let sculpture = sculptureList[index];
 
   console.log(sculpture);
   function handleNextClick() {
-    index = index + 1;
+    if (index < sculptureList.length - 1) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
   }
   function handlePreviousClick() {
-    alert("previous");
+    if (index > 0) {
+      setIndex(index - 1);
+    } else {
+      setIndex(sculptureList.length - 1);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
   return (
     <>
-      <button onClick={handleNextClick}>Next</button>
       <button onClick={handlePreviousClick}>Previous</button>
+      <button onClick={handleNextClick}>Next</button>
 
       <h2>
         <i>{sculpture.name}</i> by {sculpture.artist}
       </h2>
+
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
+
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} Details
+      </button>
+
       <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
+
+      {showMore && <p>{sculpture.description}</p>}
     </>
   );
 }
