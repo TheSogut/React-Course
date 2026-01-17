df6b5816489872f7e537a0c291e4afece262ac3a
Enes Söğüt <enessogut@outlook.com>
2026-01-17 17:03:34 +0300

    Buttons and Button Groups

--- react-mui/src/components/ButtonUsage.jsx
+++ react-mui/src/components/ButtonUsage.jsx
@@ -1,5 +1,48 @@
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import SendIcon from "@mui/icons-material/Send";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { IconButton } from "@mui/material";
 
 export default function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <>
+      <Stack direction="row" spacing={2}>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Button</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
+        <Button variant="text">Text</Button>
+        <Button variant="contained" color="success">
+          Button
+        </Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <div style={{ margin: "20px" }}>
+        <Button variant="text" size="small">
+          Text
+        </Button>
+        <Button variant="contained" size="medium" startIcon={<SendIcon />}>
+          Button
+        </Button>
+        <Button variant="outlined" size="large" endIcon={<SendIcon />}>
+          Outlined
+        </Button>
+      </div>
+
+      <div style={{ margin: "20px" }}>
+        <IconButton color="error">
+          <DeleteIcon />
+        </IconButton>
+      </div>
+
+      <ButtonGroup color="error" variant="contained" size="small">
+        <Button>One</Button>
+        <Button color="error">Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+    </>
+  );
 }
